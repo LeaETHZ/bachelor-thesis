6d2a691cde6daaa2426f52f5797f0764b486d121
LeaETHZ <lschumache@student.ethz.ch>
2025-10-24 15:24:32 +0200

    version 1.1.1. and cleaned folders, deleted clone

--- main.py
+++ main.py
@@ -1,35 +1,68 @@
-from python_motion_planning.common import *
-from python_motion_planning.path_planner import *
-from python_motion_planning.controller import *
+"""
+@file: global_examples.py
+@breif: global planner application examples
+@author: Yang Haodong, Wu Maojia
+@update: 2024.11.22
+"""
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from python_motion_planning.utils import Grid, Map, SearchFactory
 
-"""-----------------------------------------------create environment, define start/ end point-----------------------------------------------"""
+if __name__ == '__main__':
+    '''
+    path searcher constructor
+    '''
+    search_factory = SearchFactory()
 
-tree = Grid(bounds=[[0, 101], [0, 61]]) 
+    '''
+    graph search
+    '''
+    # build environment
+    start = (5, 5)
+    goal = (45, 25)
+    env = Grid(51, 31)
 
-# tree.fill_boundary_with_obstacles() # makes border of grid an obstacle
-tree.type_map[45:55, 30:36] = TYPES.OBSTACLE
-tree.type_map[75:95, 10:20] = TYPES.OBSTACLE
+    # creat planner
+    planner = search_factory("a_star", start=start, goal=goal, env=env)
+    # planner = search_factory("dijkstra", start=start, goal=goal, env=env)
+    # planner = search_factory("gbfs", start=start, goal=goal, env=env)
+    # planner = search_factory("theta_star", start=start, goal=goal, env=env)
+    # planner = search_factory("lazy_theta_star", start=start, goal=goal, env=env)
+    # planner = search_factory("s_theta_star", start=start, goal=goal, env=env)
+    # planner = search_factory("jps", start=start, goal=goal, env=env)
+    # planner = search_factory("d_star", start=start, goal=goal, env=env)
+    # planner = search_factory("lpa_star", start=start, goal=goal, env=env)
+    # planner = search_factory("d_star_lite", start=start, goal=goal, env=env)
+    # planner = search_factory("voronoi", start=start, goal=goal, env=env, n_knn=4,
+    #                             max_edge_len=10.0, inflation_r=1.0)
 
-# tree.inflate_obstacles(radius=0) # adds extra margin around obstacles
+    # animation
+    planner.run()
 
-start = (50, 0)
-goal = (50, 60)
+    # ========================================================
 
-tree.type_map[start] = TYPES.START
-tree.type_map[goal] = TYPES.GOAL
+    '''
+    sample search
+    '''
+    # # build environment
+    # start = (18, 8)
+    # goal = (37, 18)
+    # env = Map(51, 31)
 
-"""-----------------------------------------------create planner-----------------------------------------------"""
+    # # creat planner
+    # planner = search_factory("rrt", start=start, goal=goal, env=env)
+    # planner = search_factory("rrt_connect", start=start, goal=goal, env=env)
+    # planner = search_factory("rrt_star", start=start, goal=goal, env=env)
+    # planner = search_factory("informed_rrt", start=start, goal=goal, env=env)
 
-planner = AStar(map_=tree, start=start, goal=goal)
-path, path_info = planner.plan()
-print(path)
-print(path_info)
+    # # animation
+    # planner.run()
 
-"""-----------------------------------------------visualize-----------------------------------------------"""
+    # ========================================================
 
-vis = Visualizer("Path Visualizer")
-vis.plot_grid_map(tree)
-vis.plot_path(path, style="--", color="C4")
-#vis.plot_expand_tree(path_info["expand"])
-vis.show()
-vis.close()
+    '''
+    evolutionary search
+    '''
+    # planner = search_factory("aco", start=start, goal=goal, env=env)
+    # planner = search_factory("pso", start=start, goal=goal, env=env)
+    # planner.run()

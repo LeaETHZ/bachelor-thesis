303ce41bafae423abe010780a64966c8f1860da3
LeaETHZ <lschumache@student.ethz.ch>
2025-12-05 12:21:51 +0100

    obstacle range in cm

--- environment/randomize.py
+++ environment/randomize.py
@@ -57,8 +57,14 @@ def build_obstacle_ellipse(x_center: int, y_center: int, a: int, b: int, env: Gr
 class Randomize:
 
     @staticmethod
-    def random_obstacles_rectangle(env: Grid, n_obstacles: int, max_size: int = 40, min_size: int = 10) -> Grid:
+    def random_obstacles_rectangle(env: Grid, n_obstacles: int, max_size_cm: int = 20, min_size_cm: int = 4) -> Grid:
         """Generate random rectangular obstacles on a cylindrical world (x wraps)."""
+        
+        max_size = int(max_size_cm/RESOLUTION_CM)
+        min_size = int(min_size_cm/RESOLUTION_CM)
+
+        print("max_size", max_size)
+        print("max_size_cm", max_size_cm)
 
         W, H = env.x_range, env.y_range
 
@@ -77,8 +83,11 @@ class Randomize:
         return env
 
     @staticmethod
-    def random_obstacles_ellipse(env: Grid, n_obstacles: int, max_size: int = 15, min_size: int = 5) -> Grid:
+    def random_obstacles_ellipse(env: Grid, n_obstacles: int, max_size_cm: int = 15, min_size_cm: int = 5) -> Grid:
         """Generate random elliptic obstacles on a cylindrical world (x wraps)."""
+
+        max_size = int(max_size_cm/RESOLUTION_CM)
+        min_size = int(min_size_cm/RESOLUTION_CM)
 
         W, H = env.x_range, env.y_range
 
